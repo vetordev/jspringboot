3d37fa27b3d30ded5b42d0fb5a86068722cbc63c
Vitor <vitor13m2003@gmail.com>
2024-10-20 12:55:10 -0300

    setup spring boot

---
+++ src/main/java/com/github/vetordev/jspringboot/JspringbootApplication.java
@@ -0,0 +1,13 @@
+package com.github.vetordev.jspringboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JspringbootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JspringbootApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/github/vetordev/jspringboot/JspringbootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.vetordev.jspringboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JspringbootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
